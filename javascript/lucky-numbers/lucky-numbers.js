7fc544c0dc2a2a4844b72008458ca6742cce6424
Lucas Soni Teixeira <lucassonit@gmail.com>
2022-10-13 17:11:38 -0300

    test

---
+++ javascript/lucky-numbers/lucky-numbers.js
@@ -0,0 +1,36 @@
+// @ts-check
+
+/**
+ * Calculates the sum of the two input arrays.
+ *
+ * @param {number[]} array1
+ * @param {number[]} array2
+ * @returns {number} sum of the two arrays
+ */
+export function twoSum(array1, array2) {
+  return (Number(array2.join('')) + Number(array1.join('')))
+}
+
+/**
+ * Checks whether a number is a palindrome.
+ *
+ * @param {number} value
+ * @returns {boolean}  whether the number is a palindrome or not
+ */
+export function luckyNumber(value) {
+  if (value === Number(value.toString().split('').reverse().join(''))) return true
+  return false
+}
+
+/**
+ * Determines the error message that should be shown to the user
+ * for the given input value.
+ *
+ * @param {string|null|undefined} input
+ * @returns {string} error message
+ */
+export function errorMessage(input) {
+  if (input === null || input === undefined || input === '') return 'Required field'
+  if (!Boolean(Number(input))) return 'Must be a number besides 0'
+  return ''
+}
